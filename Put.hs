df7c1a4305080113b598da03e7f80f70226c9137
Bercovici Adrian Simon <adriansan_93@yahoo.com>
2018-06-13 07:29:07 +0300

    Resolved

--- Put.hs
+++ Put.hs
@@ -4,6 +4,7 @@ module Put(
 ) where
     import Data.List
     import Jlib
+  
 
 
     sample=JArray[
@@ -32,3 +33,24 @@ module Put(
     kvString::(String,JValue)->String
     kvString ([],_)=""
     kvString (f,l)=f++":"++renderJValue l
+    sample=JArray[
+                    JObject [("name",JString "adita"),("age",JNumber 13)],
+                    JObject [("name",JString "dan"),("kids",JNumber 3)] ,
+                    JNumber 3,
+                    JBool False,
+                    JString "Howdy"
+                    ]
+
+    renderJValue::JValue->String
+    renderJValue (JString s)= s
+    renderJValue (JNumber nr)=show nr
+    renderJValue (JBool True)="True"
+    renderJValue(JBool False)="False"
+    renderJValue JNull="Null"
+    renderJValue (JObject o)="{"++kvpairs o++"}" where
+        kvpairs []= ""
+        kvpairs ls=intercalate "," (map renderPair ls) 
+        renderPair (k,v)=show k++":"++renderJValue v
+    renderJValue (JArray a)="["++renderArr a++"]" where
+        renderArr []=""
+        renderArr arr=intercalate "," (map renderJValue arr)
--- script.sh
+++ script.sh
@@ -1,5 +1,6 @@
 echo "Compiling Haskell program"
+ghc -c Pretty.hs
 ghc -c Jlib.hs
 ghc -c Main.hs
 ghc -c Put.hs
-ghc -o Main Main.o Jlib.o Put.o
+ghc -o Main Main.o Pretty.o Jlib.o Put.o 
